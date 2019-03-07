371491cac32112475f71a5cf83b28aba852e6d63
Manoj Upreti <manoj-u@HCL.COM>
2019-03-07 21:50:26 +0530

    Created login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+ 	  AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase : {
+    apiKey: "AIzaSyDMzdp36Miql-FsCwLU_Q4oS5N0oK_kx6U",
+    authDomain: "arth-e37f3.firebaseapp.com",
+    databaseURL: "https://arth-e37f3.firebaseio.com",
+    projectId: "arth-e37f3",
+    storageBucket: "arth-e37f3.appspot.com",
+    messagingSenderId: "541018238644"
+  }
 };
