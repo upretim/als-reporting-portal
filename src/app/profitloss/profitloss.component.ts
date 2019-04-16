1abae5cb73feb06158f1db41e67a8824722581ec
Manoj Upreti <manoj-u@HCL.COM>
2019-04-16 22:12:43 +0530

    Added PL sheet logic

--- src/app/profitloss/profitloss.component.ts
+++ src/app/profitloss/profitloss.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../data.service';
+import { Iinvoice } from '../model/model';
 
 @Component({
   selector: 'app-profitloss',
@@ -7,9 +8,18 @@ import {DataService} from '../data.service';
   styleUrls: ['./profitloss.component.css']
 })
 export class ProfitlossComponent implements OnInit {
-
+  invoiceData: Iinvoice[];
   clients: any;
   clientList: any;
+  hasSubClient = false;
+  subClientList: [] = [];
+  totalValue: number = 0;
+  numberOfInv: number = 0;
+  hasDataToDisplay: boolean = true;
+  selectedPage: number =1;
+  filterObj: any = {
+  }
+
   constructor(private dataService: DataService) { 
     this.clientList = this.dataService.data.clientsList;
   }
@@ -20,7 +30,47 @@ export class ProfitlossComponent implements OnInit {
     console.log(event);
   }
 
-  selectChangeClient(event){
-    console.log(event);
+ 
+
+  selectChangeClient(event) {
+    this.selectedPage = 1;
+    this.hasSubClient = false;
+    let selectedClientId = event.currentTarget.value;
+    this.filterObj.billedTo = selectedClientId;
+    this.filterObj.subclientId = "";
+    if (event.currentTarget.value != "") {
+      let selectedClient = this.clientList.filter( (val)=> {
+        return val.clientId == event.currentTarget.value;
+      });
+      if (selectedClient[0].hasChild) {
+        this.hasSubClient = true;
+        this.subClientList = selectedClient[0].childs;
+      }
+    }
+    this.filterData();  
+  }
+
+  filterData(){
+    this.invoiceData = this.multiFilter(this.dataService.data.invoice, this.filterObj);
+    this.totalValue = this.invoiceData.reduce( (accumulator, invoice) => {
+      return accumulator + invoice.amount;
+    }, 0);
+    this.hasDataToDisplay = true;
+    if (this.invoiceData.length == 0) {
+      this.hasDataToDisplay = false;
+    }
+  }
+
+  multiFilter(array, filters) {
+    const filterKeys = Object.keys(filters);
+    // filters all elements passing the criteria
+    return array.filter((item) => {
+      // dynamically validate all filter criteria
+      return filterKeys.every(key => {
+        // ignores an empty filter
+        if (!filters[key].length) return true;
+        return filters[key].includes(item[key]);
+      });
+    });
   }
 }
